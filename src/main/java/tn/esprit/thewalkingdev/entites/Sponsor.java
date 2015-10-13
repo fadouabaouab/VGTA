8c14f5843b6eb848dd2f356bdb3ea6acaa14c708
anesmaz <medanes.mazouni@esprit.tn>
2015-10-13 20:09:10 +0200

    fix conflict

--- src/main/java/tn/esprit/thewalkingdev/entites/Sponsor.java
+++ src/main/java/tn/esprit/thewalkingdev/entites/Sponsor.java
@@ -2,6 +2,7 @@ package tn.esprit.thewalkingdev.entites;
 
 import java.io.Serializable;
 import java.lang.String;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.*;
@@ -11,24 +12,59 @@ public class Sponsor implements Serializable {
 
 	private int id_sponsor;
 	private String name_sponsor;
-	
+	private TeamSponsoring teamSponsoring;
 	private List<Contribution> contributions;
 	private static final long serialVersionUID = 1L;
-
+  private String name_contact_sponsor;
+  private String email;
+  private Date dateStart;
+  private Date dateEnd;
 	public Sponsor() {
 	}
 	
 	public Sponsor(int id_sponsor, String name_sponsor,
-			 List<Contribution> contributions) {
+			TeamSponsoring teamSponsoring, List<Contribution> contributions,
+			String name_contact_sponsor, String email) {
 		super();
 		this.id_sponsor = id_sponsor;
 		this.name_sponsor = name_sponsor;
-		
+		this.teamSponsoring = teamSponsoring;
 		this.contributions = contributions;
+		this.name_contact_sponsor = name_contact_sponsor;
+		this.email = email;
+	}
+
+	public Sponsor(String name_sponsor,
+			TeamSponsoring teamSponsoring, List<Contribution> contributions,
+			String name_contact_sponsor, String email) {
+		super();
+		
+		this.name_sponsor = name_sponsor;
+		this.teamSponsoring = teamSponsoring;
+		this.contributions = contributions;
+		this.name_contact_sponsor = name_contact_sponsor;
+		this.email = email;
 	}
 
 
 
+
+
+//	@Override
+//	public String toString() {
+//		return "Sponsor [id_sponsor=" + id_sponsor + ", name_sponsor="
+//				+ name_sponsor + ", teamSponsoring=" + teamSponsoring
+//				+ ", contributions=" + contributions
+//				+ ", name_contact_sponsor=" + name_contact_sponsor + ", email="
+//				+ email + "]";
+//	}
+
+	@Override
+	public String toString() {
+		return "Sponsor [id_sponsor=" + id_sponsor + ", name_sponsor="
+				+ name_sponsor + ", name_contact_sponsor="
+				+ name_contact_sponsor + ", email=" + email + "]";
+	}
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -48,6 +84,15 @@ public class Sponsor implements Serializable {
 		this.name_sponsor = name_sponsor;
 	}
 	
+	@ManyToOne
+	@JoinColumn(name = "teamSponsoring")
+	public TeamSponsoring getTeamSponsoring() {
+		return teamSponsoring;
+	}
+
+	public void setTeamSponsoring(TeamSponsoring teamSponsoring) {
+		this.teamSponsoring = teamSponsoring;
+	}
 
 	@OneToMany(mappedBy="sponsor")
 	public List<Contribution> getContributions() {
@@ -56,6 +101,40 @@ public class Sponsor implements Serializable {
 
 	public void setContributions(List<Contribution> contributions) {
 		this.contributions = contributions;
+	}
+
+	public String getName_contact_sponsor() {
+		return name_contact_sponsor;
+	}
+
+	public void setName_contact_sponsor(String name_contact_sponsor) {
+		this.name_contact_sponsor = name_contact_sponsor;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public Date getDateStart() {
+		return dateStart;
+	}
+
+	public void setDateStart(Date dateStart) {
+		this.dateStart = dateStart;
+	}
+
+	public Date getDateEnd() {
+		return dateEnd;
+	}
+
+	public void setDateEnd(Date dateEnd) {
+		this.dateEnd = dateEnd;
 	}
 
 	
